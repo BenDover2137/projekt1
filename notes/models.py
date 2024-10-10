57ebb3fb1a26bdc323328b161e8cf6757214a9f5
student08 <student@gmail.com>
2024-10-10 11:50:55 +0200

    css

---
+++ notes/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+from django.conf import settings
+from django.db import models
+
+class Note(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.title
