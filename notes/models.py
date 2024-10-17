6aa5159b42ee4759cc0d10757f95f94c67dc182e
student08 <student@gmail.com>
2024-10-17 12:12:05 +0200

    waznosc

--- notes/models.py
+++ notes/models.py
@@ -1,19 +1,20 @@
 from django.db import models
+from django.template.defaultfilters import length
 from django.urls import reverse
 # Create your models here.
 from django.conf import settings
 from django.db import models
 
 class Note(models.Model):
-    STATUS_CHOICES = ((1, 'malo_wazne'),
-                      (2, 'srednio_wazne'),
-                      (3  , 'bardzo_wazne'),)
+    STATUS_CHOICES = (( '1','malo wazne'),
+                      ( '2','srednio wazne'),
+                      ( '3','bardzo wazne'),)
 
     title = models.CharField(max_length=50)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    importance = models.IntegerField(choices=STATUS_CHOICES, default='1')
+    importance = models.CharField(max_length=13,choices=STATUS_CHOICES, default='1')
 
     def __str__(self):
         return self.title
